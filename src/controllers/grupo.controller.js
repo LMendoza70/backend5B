1393a0fb3bf5ea05620587968aada099288dd244
@LMendoza70 <luis.mendoza@uthh.edu.mx>
2026-02-25 12:02:24 -0600

    feat: implement grupo controller and model with CRUD operations

---
+++ src/controllers/grupo.controller.js
@@ -0,0 +1,32 @@
+import * as grupoModelo from '../models/grupo.model.js'
+
+export const getAllGrupos = async (req,res)=>{
+    try {
+        const grupos = await grupoModelo.getAllGrupos();
+        res.status(200).json(grupos)   
+    } catch (error) {
+        res.status(500).json({error:error.message})
+    }
+}
+
+export const getGrupoById = async (req,res)=>{
+    try {
+        const grupo = await grupoModelo.getGrupoById(req.params.id)
+        if(!grupo)
+            res.status(404).json({message:'Como el amor de ella ***no encontrado :´( ***'})
+        res.status(200).json(grupo)
+    } catch (error) {
+        res.status(500).json({error:error.message})
+    }
+}
+
+export const createGrupo = async (req,res)=>{
+    try {
+        if(!req.body.nombre_grupo)
+            res.status(400).json({message:'No fue agregado el nombre'})
+        const nuevo= await grupoModelo.createGrupo(req.body)
+        res.status(201).json(nuevo)
+    } catch (error) {
+        res.status(500).json({error: error.message})
+    }
+}
---
+++ src/models/grupo.model.js
@@ -0,0 +1,18 @@
+import db from '../config/DB.js'
+
+export const getAllGrupos= async ()=>{
+    const [rows]= await db.query('select * from grupos where activo=1')
+    return rows
+}
+
+export const getGrupoById= async (id)=>{
+    const [rows]= await db.query('select * from grupos where activo=1 and id_grupo=?',[id])
+    return rows[0]
+}
+
+export const createGrupo = async({nombre_grupo, descripcion, activo})=>{
+    const [restult]=await db.query(
+        'insert into grupos(nombre_grupo, descripcion, activo) values (?,?,?)'
+        ,[nombre_grupo,descripcion || null,activo || null])
+    return {id:restult.insertId, nombre_grupo, descripcion, activo}
+}
