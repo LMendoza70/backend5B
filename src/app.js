23123f450d76e6f4a8a084abec2ff63ff436499a
@LMendoza70 <luis.mendoza@uthh.edu.mx>
2026-02-27 09:36:47 -0600

    feat: update app.js to configure dotenv and improve server setup

--- src/app.js
+++ src/app.js
@@ -1,13 +1,24 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import grupoRoutes from './routes/grupos.routes.js'
 
+//creamos la instancia de nuestro servidor 
 const app=express();
+//definimos la variables de entrono a utilizar 
+dotenv.config()
+const port = process.env.PORT || 3000
 
-const PORT=3000;
+//definimos el midleware para trabajar con json 
+app.use(express.json())
+
+//agregamos las rutas con las que voy a trabajar 
+//rutas de grupos 
+app.use('/api/grupos',grupoRoutes)
 
 app.get('/',(req,res)=>{
     res.send("Esta es mi primer API... 5B ")
 })
 
-app.listen(PORT,()=>{
-    console.log("Aplicacion de express corriendo en el puerto "+PORT)
+app.listen(port,()=>{
+    console.log("Aplicacion de express corriendo en el puerto "+port)
 })
